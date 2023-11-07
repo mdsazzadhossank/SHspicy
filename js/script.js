d200d3c231ff56b0850bed454e1c09a2cec9341d
sazzad018 <ronykazi018@gmail.com>
2023-11-07 16:35:04 +0600

    slider add

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// menu 
+let menu =document.querySelector('.main_menu')
+
+window.addEventListener('scroll',()=>{
+    let scrolling = this.scrollY
+    if(scrolling > 200){
+        menu.classList.add('menu_fixed')
+    }
+    else{
+        menu.classList.remove('menu_fixed') 
+    }
+})
+
+
+// slider
+
+$('.slider_cards').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots:true
+  });
